b65f79e2fd89807aeb8eadb7cff1c9c2a4ae18e0
borys <borysweiner@gmail.com>
2022-03-29 14:50:26 +0200

    finished game, still unpolished; added assets file to clean up

--- python/hangmanGame.py
+++ python/hangmanGame.py
@@ -1,91 +1,40 @@
 import random
+from hangmanAssets import wordBank, drawings
 
-drawings = ['''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
+chosenWord = list(random.choice(wordBank))
+hashedWord = ["_" for i in chosenWord]
+failedTries = 0
+numOfRounds = 1
+triedLetters = []
 
-wordBank = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
-         'coyote crow deer dog donkey duck eagle ferret fox frog goat '
-         'goose hawk lion lizard llama mole monkey moose mouse mule newt '
-         'otter owl panda parrot pigeon python rabbit ram rat raven '
-         'rhino salmon seal shark sheep skunk sloth snake spider '
-         'stork swan tiger toad trout turkey turtle weasel whale wolf '
-         'wombat zebra ').split()
+print("Category: animals.")
 
-
-def draw_word():
-    return random.choice(wordBank)
-
-
-chosenWord = list(draw_word())
-
-hashedWord = ["_" for i in list(chosenWord)]
-
-triesCounter = 0
-
-for i in range(7):
+# The game itself
+while failedTries != 7:
+    print(f"|||||||||||\n||ROUND {numOfRounds}||\n|||||||||||")
+    print("Tried letters:")
+    print(*triedLetters, sep=", ")
     print(*hashedWord)
     guessedLetter = input("Type in single letter you think is in the word and confirm with 'enter':\n")
+    if guessedLetter in triedLetters:
+        print("You already tried that one.")
+        continue
+    else:
+        triedLetters.append(guessedLetter)
     if guessedLetter in chosenWord:
         instances = [x for x, z in enumerate(chosenWord) if z == guessedLetter]
         for j in instances:
             hashedWord[j] = guessedLetter
     else:
-        print(drawings[triesCounter])
-        triesCounter += 1
+        print(drawings[failedTries])
+        failedTries += 1
+    numOfRounds += 1
+    if "_" not in hashedWord:
+        break
 
-
-
-
-
-print(chosenWord)
-print(hashedWord)
-print(*hashedWord)
-
+if failedTries == 7:
+    print("Sorry, you lost.")
+    print("You were looking for a word", "".join(chosenWord))
+else:
+    print(*hashedWord)
+    print("GG, you've won.")
